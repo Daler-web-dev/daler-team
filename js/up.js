1d7b35245d7c1ab2aee30cfdbcded06ff3520f23
daler <exaple@gmail.com>
2021-01-06 09:03:08 +0500

    first commit

---
+++ js/up.js
@@ -0,0 +1,7 @@
+const answers = []
+
+answers[0] = prompt('what is your name?', '')   
+answers[1] = prompt('what is your name?', '')
+answers[2] = prompt('what is your name?', '')
+
+document.write(answers)
